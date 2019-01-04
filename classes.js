8015121772943aa1e608046fcc80ee52e9211503
Renniej <tai.juan2@gmail.com>
2019-01-04 07:59:45 -0500

    Classes

---
+++ classes.js
@@ -0,0 +1,16 @@
+//After typescript is converted to Javascript
+
+var Person = /** @class */ (function () {
+    function Person() {
+    }
+    return Person;
+}());
+;
+var aPerson = new Person; //Implicity sets aPerson to type Person
+aPerson.FirstName = "Tai";
+aPerson.LastName = "Rennie";
+aPerson.random = 10;
+aPerson.random = "TEST";
+aPerson.random = true;
+console.log(aPerson.FirstName + " " + aPerson.LastName);
+var aPerson2 = new Person; //Explicity sets aPerson2 to type Person
---
+++ classes.ts
@@ -0,0 +1,21 @@
+
+class Person { //similar to how you define clsss in c++
+
+    FirstName : string;
+    LastName : string;
+    random; //can be any type
+
+};
+
+
+var aPerson = new Person; //Implicity sets aPerson to type Person
+aPerson.FirstName = "Tai";
+aPerson.LastName = "Rennie";
+aPerson.random = 10;
+aPerson.random = "TEST";
+aPerson.random = true;
+
+console.log(`${aPerson.FirstName} ${aPerson.LastName}`);
+
+
+var aPerson2 : Person = new Person; //Explicity sets aPerson2 to type Person
